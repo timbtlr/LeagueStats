5644595fd68082a7a5941553b47f9301a497120c
Tym <tim.butler@trackmaven.com>
2016-03-29 15:02:46 -0400

    Fixed some weird folder issues

--- match/management/commands/get_matches.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from match.tasks import schedule_pull_matches
-
-class Command(BaseCommand):
-    help = 'Starts a task to pull match data for all summoners'
-
-    def handle(self, *args, **options):
-        schedule_pull_matches()
